f5171cbd6b455e579abdd3e4f6b066da7087448b
Florian Ziesche <florian.ziesche@gmail.com>
2013-04-12 00:57:29 +0200

    * merged tcc.c and libtcc.c -> removed tcc.c

--- libtcc.c
+++ libtcc.c
@@ -718,3 +718,76 @@ PUB_FUNC void tcc_print_stats(TCCState *s, int64_t total_time)
            tt, (int)(total_lines / tt),
            total_bytes / tt / 1000000.0);
 }
+
+/********************************************************/
+/* standalone binary */
+
+#if !defined(TCC_LIB_ONLY)
+
+static void help(void)
+{
+    printf("tcc version " TCC_VERSION " - Tiny C Compiler - Copyright (C) 2001-2006 Fabrice Bellard\n"
+           "Usage: tcc [options...] infile(s)...\n"
+           "General options:\n"
+           "  -v          show version\n"
+           "Preprocessor options:\n"
+           "  -E          preprocess only\n"
+           "  -Idir       add include path 'dir'\n"
+           "  -Dsym[=val] define 'sym' with value 'val'\n"
+           "  -Usym       undefine 'sym'\n"
+           );
+}
+
+int main(int argc, const char* argv[]) {
+    TCCState *s;
+    int ret, optind, i;
+    const char *first_file = NULL;
+	
+    s = tcc_new();
+    s->output_type = TCC_OUTPUT_EXE;
+	
+    optind = tcc_parse_args(s, argc - 1, argv + 1);
+	
+    if (optind == 0) {
+        help();
+        return 1;
+    }
+	
+    if (s->verbose)
+        printf("tcc version %s\n", TCC_VERSION);
+	
+    if (s->verbose && optind == 1)
+        return 0;
+	
+    if (s->nb_files == 0)
+        tcc_error("no input files\n");
+    
+    if (s->output_type == TCC_OUTPUT_PREPROCESS) {
+        s->ppfp = stdout;
+    }
+	
+    /* compile or add each files or library */
+    for(i = ret = 0; i < s->nb_files && ret == 0; i++) {
+        const char *filename;
+		
+        filename = s->files[i];
+		if (1 == s->verbose)
+			printf("-> %s\n", filename);
+		if (tcc_add_file(s, filename) < 0)
+			ret = 1;
+		if (!first_file)
+			first_file = filename;
+    }
+	
+    if (0 == ret) {
+        if (s->output_type == TCC_OUTPUT_MEMORY) {
+            tcc_error_noabort("-run is not available");
+            ret = 1;
+        }
+    }
+	
+    tcc_delete(s);
+    return ret;
+}
+
+#endif
