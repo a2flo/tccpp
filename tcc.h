b1e629a17580b6e84a0762f159b6e0a1d4e80042
Florian Ziesche <florian.ziesche@gmail.com>
2013-04-13 17:56:34 +0200

    * removed unnecessary uint32_t and uint64_t typedefs

--- tcc.h
+++ tcc.h
@@ -66,8 +66,6 @@
 
 typedef unsigned char           uint8_t;
 typedef unsigned short int      uint16_t;
-typedef unsigned int            uint32_t;
-typedef unsigned long long int  uint64_t;
 
 #include "libtcc.h"
 
